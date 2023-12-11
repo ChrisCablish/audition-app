80b633decd784e8cf1e831bf04fb93da566b89db
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2023-12-11 09:14:37 -0600

    initial commit

---
+++ src/main/java/com/example/auditionapp/AuditionappApplication.java
@@ -0,0 +1,13 @@
+package com.example.auditionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditionappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auditionapp/AuditionappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auditionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditionappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
