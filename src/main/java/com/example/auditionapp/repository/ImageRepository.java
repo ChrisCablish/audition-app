9dc1dfab5f2e0fb5ccf272012907c78e76c0ed65
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2024-01-06 10:19:43 -0600

    create image repository

---
+++ src/main/java/com/example/auditionapp/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.auditionapp.repository;
+
+import com.example.auditionapp.model.Image;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image,Long> {
+}
